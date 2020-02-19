910063e84d791e65da96e24f74f80ae6a97462e2
justrmarks <blackbirdwanderer@gmail.com>
2020-02-19 14:55:50 -0600

    using stringIO to fake IO object for google authorizer

--- app/models/concerns/google_calendar_wrapper.rb
+++ app/models/concerns/google_calendar_wrapper.rb
@@ -5,11 +5,9 @@ require 'stringio'
 class GoogleCalendarWrapper
    
     def initialize
-        sio = StringIO.new
-        sio.print(ENV['google_secrets'])
         scopes = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events']
         authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
-        json_key_io: sio, #File.open('/Users/shedaddy/Development/Groove_Cafe_capstone/project-sploopy-api/secrets/project-sploopy-d9c223115cef.json'),
+        json_key_io: StringIO.new(ENV['google_secrets'], #File.open('/Users/shedaddy/Development/Groove_Cafe_capstone/project-sploopy-api/secrets/project-sploopy-d9c223115cef.json'),
         scope: scopes)
         authorizer.fetch_access_token!
         @client = Google::Apis::CalendarV3::CalendarService.new
